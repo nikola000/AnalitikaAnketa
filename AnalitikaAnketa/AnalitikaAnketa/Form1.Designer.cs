59901ede9cc108c49739211a9398f7158249c15b
Nikola Banicevic <nikola.banicevic2012@gmail.com>
2021-07-03 21:39:42 +0200

    Izbacio sam dugme

--- AnalitikaAnketa/AnalitikaAnketa/Form1.Designer.cs
+++ AnalitikaAnketa/AnalitikaAnketa/Form1.Designer.cs
@@ -29,22 +29,12 @@ namespace AnalitikaAnketa
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
             this.tbUser = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(585, 315);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // menuStrip1
             // 
@@ -87,7 +77,6 @@ namespace AnalitikaAnketa
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tbUser);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -100,8 +89,6 @@ namespace AnalitikaAnketa
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
         private System.Windows.Forms.Label label1;
