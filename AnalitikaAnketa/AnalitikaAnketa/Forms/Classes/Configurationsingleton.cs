362e305332d9120582b151227183c7801773b71e
Andrija Rajovic <andrijaogi@gmail.com>
2021-07-08 15:30:52 +0200

    Configuration klasa

---
+++ AnalitikaAnketa/AnalitikaAnketa/Forms/Classes/Configurationsingleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalitikaAnketa.Forms.Classes
+{
+    class Configurationsingleton
+    {
+        public string ConnectionString { get; set; }
+        public static Configurationsingleton single_instance = null;
+        public static Configurationsingleton getInstance()
+        {
+            if (single_instance == null)
+            { single_instance = new Configurationsingleton(); }
+            return single_instance;
+        }
+    }
+}
